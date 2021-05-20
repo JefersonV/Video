2109a2aa38a799c0242999e01abeda9c7e5f9a53
JefersonV <jvelasquezc15@miumg.edu.gt>
2021-05-20 13:29:37 -0600

    Estructura inicial del primer componente

---
+++ src/components/Header.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import '../assets/styles/App.scss';
+// import logo from '../assets/images/logo-platzi-video-BW2.png';
+// import userIcon from '../assets/images/user-icon';
+
+class Header extends React.Component {
+	render() {
+		return (
+		  <header className="header">
+			  <img className="header__img" src="" alt="Platzi Video" />
+
+				<div className="header__menu">
+
+					<div className="header__menu--profile">
+						<img src="" alt="" />
+						<p>Perfil</p>
+					</div>
+
+					<ul>
+						<li><a href="">Cuenta</a></li>
+						<li><a href="">Cerrar Sesión</a></li>
+					</ul>
+				</div>
+		  </header>
+		)
+	}
+}
+
+export default Header;
