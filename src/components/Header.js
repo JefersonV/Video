e74178efa6cf2cace0943e070297f72194249579
JefersonV <jvelasquezc15@miumg.edu.gt>
2021-05-20 23:20:59 -0600

    componente <header> hecho

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,18 @@
 import React from 'react';
-import '../assets/styles/App.scss';
-// import logo from '../assets/images/logo-platzi-video-BW2.png';
-// import userIcon from '../assets/images/user-icon';
+import '../assets/styles/components/Header.scss';
+import logo from '../assets/images/logo-platzi-video-BW2.png';
+import userIcon from '../assets/images/user-icon.png';
 
 class Header extends React.Component {
 	render() {
 		return (
 		  <header className="header">
-			  <img className="header__img" src="" alt="Platzi Video" />
+			  <img className="header__img" src={logo} alt="Platzi Video" />
 
 				<div className="header__menu">
 
 					<div className="header__menu--profile">
-						<img src="" alt="" />
+						<img src={userIcon} alt="" />
 						<p>Perfil</p>
 					</div>
 
