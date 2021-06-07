d6a7fabc6c43b68ba570779e9f347e6a60073e57
Jeferson Velásquez <jvelasquezc15@miumg.edu.gt>
2021-06-07 16:58:26 -0600

    finalizado, hay que agregar un README

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import '../assets/styles/components/Main.scss';
 class Main extends React.Component {
-    render () {
-        return (
-        <section className="main">
-            <h2 className="main__title">Qué quieres ver hoy?</h2>
-            <input type="text" className="input" placeholder="buscar..."/>
-        </section>
-        )
-    }
+	render () {
+		return (
+		<section className="main">
+			<h2 className="main__title">Qué quieres ver hoy?</h2>
+			<input type="text" className="input" placeholder="buscar..."/>
+		</section>
+		)
+	}
 }
 
 export default Main;
